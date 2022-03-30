5120bbfc21dc5cdc136493573d4d2b52f3f5515d
stella-ewerton <stellaewerton84@gmail.com>
2022-03-30 10:57:32 +0100

    Cabeçalho

---
+++ wp-content/plugins/stella/stella.php
@@ -0,0 +1,11 @@
+<?php
+
+* Plugin Name: stella
+* Plugin URI: https://github.com/stella-ewerton/Alfasoft
+* Description: Header
+* Version: 1.0.0
+* Author: Stella Ewerton
+* Author URI: https://github.com/stella-ewerton/Alfasoft
+* License: GPL2
+?>
+
